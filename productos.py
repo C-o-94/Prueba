c301dc20a3078cd5c78a16eed90352b838cc0adc
Constanza Osorio <co.osoriou@duocuc.cl>
2024-06-28 20:16:46 -0400

    Prueba 3

--- productos.py
+++ productos.py
@@ -1,9 +1,12 @@
+from menu import productos
+
 
 def generar_precio():
 
 
 def agregar_producto():
     precio = 
-    nombre =
-    cantidad =
-    categoria =
+    nombre = imput("Ingrese el nombre")
+    cantidad = int(imput("Ingrese la cantidad del producto"))
+    categoria = imput("Ingrese la categoria del producto")
+    
